559e1b993fa808607b43983698c3afc66684b9ee
Gian Maria Ricci <alkampfer@nablasoft.com>
2020-05-16 11:59:03 +0200

    Added basic file logging capabilities
    As for story #38672

---
+++ src/FantasticLogLibrary.FileDestination/SingleFileLogDestination.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FantasticLogLibrary.FileDestination
+{
+    public class SingleFileLogDestination : ILogDestination
+    {
+        private FileStream _fileStream;
+        private StreamWriter _streamWriter;
+
+        public SingleFileLogDestination(String fileName)
+        {
+            _fileStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+            _fileStream.Seek(0, SeekOrigin.End);
+            _streamWriter = new StreamWriter(_fileStream);
+        }
+
+        public void AddLog(LogMessage message)
+        {
+            _streamWriter.Write($"{message.Level}[{message.Timestamp.ToString("s")}] - {message.RenderedMessage}\n");
+        }
+
+        public void Flush()
+        {
+            _streamWriter.Flush();
+        }
+    }
+}
