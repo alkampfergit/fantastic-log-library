114d296eeb73f9b11e6adbd10fc52404c00c9f5b
alkampfer <alkampfer@nablasoft.com>
2020-05-18 11:33:48 -0700

    added test for log in mongo #38671

---
+++ src/FantasticLogLibrary.Tests/MongoLogDestinationTests.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasticLogLibrary;
+using NUnit.Framework;
+
+namespace FantasticLogLibrary.Tests
+{
+    public class MongoLogDestinationTests
+    {
+        private IMongoCollection<MongoLog> _coll;
+        private MongoLogDestination _sut;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            string connection = "mongodb://localhost/test-logger";
+            var url = new MongoUrl(connection);
+            var client = new MongoClient(url);
+            var db = client.GetDatabase(url.DatabaseName);
+            _coll = db.GetCollection<MongoLog>("log");
+            db.DropCollection("log");
+
+            _sut = new MongoLogDestination(connection, "log");
+        }
+
+
+        [Test]
+        public void basic_save_of_log()
+        {
+            _sut.AddLog(new LogMessage(LogLevel.Info, "test", DateTime.UtcNow));
+            Assert.That(_coll.AsQueryable().Count(), Is.EqualTo(1)); 
+        }
+    }
+}
