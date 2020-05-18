4069133ef43c3c7f0fe9b95fb0f2b58a9bc7297d
alkampfer <alkampfer@nablasoft.com>
2020-05-18 11:32:45 -0700

    added log in mongo #38671

---
+++ src/FantasticLogLibrary/MongoLogDestination.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasticLogLibrary
+{
+    public class MongoLogDestination : ILogDestination
+    {
+        private IMongoCollection<MongoLog> Collection;
+
+        public MongoLogDestination(String mongoConnection, String collectionName)
+        {
+            var url = new MongoUrl(mongoConnection);
+            var client = new MongoClient(url);
+            var db = client.GetDatabase(url.DatabaseName);
+            Collection = db.GetCollection<MongoLog>(collectionName);
+        }
+
+        public void AddLog(LogMessage message)
+        {
+            Collection.InsertOne(new MongoLog(message));
+        }
+    }
+
+    public class MongoLog
+    {
+
+        public MongoLog(LogMessage log)
+        {
+            Id = ObjectId.GenerateNewId();
+            Log = log;
+        }
+
+        public ObjectId Id { get; set; }
+
+        public LogMessage Log { get; set; }
+    }
+}
